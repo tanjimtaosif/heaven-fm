9c7166a3104e713d0a5eadda6c8049432ff732eb
tanjimtaosif <itsmahtab007@gmail.com>
2026-09-06 07:58:31 +0600

    feat(home): add ScrollVideoRevealSection component

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+export { COMPANY_INFO } from './companyData'
+export {
+  PRODUCTS,
+  FEATURED_PRODUCTS,
+  PRODUCT_CATEGORIES,
+  SORT_OPTIONS,
+  PRICE_RANGES,
+  BESPOKE_PRODUCTS,
+  filterProducts,
+  getProductById,
+  getProductsByCategory,
+} from './productsData'
+export { FAQ_CATEGORIES, FAQS } from './faq'
+export { TESTIMONIALS } from './reviewsData'
