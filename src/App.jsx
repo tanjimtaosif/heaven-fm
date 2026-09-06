f90707cfc6b47fcdd9e19f11e395c7405a506900
tanjimtaosif <itsmahtab007@gmail.com>
2026-09-06 17:50:21 +0600

    feat(app): wrap application root with QuotationProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { SmoothScrollProvider } from '@/components/providers'
 import { CartProvider } from '@/features/cart'
+import { QuotationProvider } from '@/features/quotation'
 import { AppRoutes } from '@/routes'
 
 export default function App() {
   return (
     <SmoothScrollProvider>
       <CartProvider>
-        <AppRoutes />
+        <QuotationProvider>
+          <AppRoutes />
+        </QuotationProvider>
       </CartProvider>
     </SmoothScrollProvider>
   )
