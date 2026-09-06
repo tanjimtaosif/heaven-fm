566db81f37ab2331161555a135ea7f76deb55f55
tanjimtaosif <itsmahtab007@gmail.com>
2026-09-06 07:59:30 +0600

    refactor(lib): clean utility helper comments

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,9 +1,1 @@
-import { clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-/**
- * Merges Tailwind classes conditionally and resolves conflicts cleanly.
- */
-export function cn(...inputs) {
-  return twMerge(clsx(inputs))
-}
+export { cn } from '@/utils'
